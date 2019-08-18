fa0522c44607f2af4c36d35bf5365c0c6400abcf
Summair <48170270+Summair@users.noreply.github.com>
2019-08-18 15:07:22 -0500

    Legacy Rest
    built tested on Pivotal tc Server v3.1
    {"message":"Hello World!"}
    
    Can change annotation in main HomeController 
    to @ResponseBody

---
+++ Legacy_REST/src/main/java/com/summair/demo/HomeController.java
@@ -0,0 +1,43 @@
+package com.summair.demo;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@ResponseBody
+
+public class HomeController {
+	
+	@RequestMapping ("/")
+	
+	public Greet sayHello() {
+		
+		return new Greet("Hello World!");
+	}
+}
+class Greet {
+	private String message;
+	public Greet(String message) {
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
